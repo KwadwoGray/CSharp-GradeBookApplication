d63755fc1ca00cfa52b0284cd4f910059845fa10
Gray <KMG0425@daugherty.com>
2022-05-19 14:17:19 -0500

    Adding code to handle a grade of A

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+
 
 namespace GradeBook.GradeBooks {
     public class RankedGradeBook : BaseGradeBook {
@@ -11,11 +13,34 @@ namespace GradeBook.GradeBooks {
 
         public override char GetLetterGrade(double averageGrade)
         {
-            //return base.GetLetterGrade(averageGrade);
+            double gradeThreshold = Students.Count * 0.2;
+            List<double> allGrades = new List<double>();
+
+            allGrades = GetStudentsGrades(Students);
+            allGrades.Sort();
+            
             if (Students.Count < 5) {
-                throw new ArgumentException("Need more than 5 students");
+                throw new InvalidOperationException("Ranked grading requires a minimum of 5 students");
             }
+
+            double AGradeThreshold = allGrades[(int)Math.Ceiling(gradeThreshold)];
+
+            if (averageGrade > AGradeThreshold){
+                return 'A';
+            }
+
             return 'F';
+
+            //return base.GetLetterGrade(averageGrade);
+        }
+
+        private List<double> GetStudentsGrades(List<Student> students)
+        {
+            List<double> studentGrades = new List<double>();
+            foreach (var student in students) {
+                studentGrades.Add(student.AverageGrade);
+            }
+            return studentGrades;
         }
     }
 }
